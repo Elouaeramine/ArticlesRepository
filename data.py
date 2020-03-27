bcd981f313c901f429aadb19caef5f9aad85d6cd
Elouaeramine <49250202+Elouaeramine@users.noreply.github.com>
2020-03-27 02:17:17 +0100

    first commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title':'Article One',
+            'body':'just came here to tell you that you can be something there in the place you live, here you are just one more member, with the difference that you really need a lot of attention, now I will silence you just for you read this and in exactly 30 minutes you will be banned, and you can do - nothing...',
+            'author': 'Juliano Carneiro',
+            'create_date': '03-25-2020'
+        },
+        {
+            'id':2,
+            'title':'Article Two',
+            'body':'Classification problems having multiple classes with imbalanced dataset present a different challenge than a binary classification problem. The skewed distribution makes many conventional machine learning algorithms less effective, especially in predicting minority class examples. In order to do so, let us first understand the problem at hand and then discuss the ways to overcome those.',
+            'author': 'Antonio Banderas',
+            'create_date': '07-14-2005'
+        },
+        {
+            'id':3,
+            'title':'Article Three',
+            'body':'Classification problems having multiple classes with imbalanced dataset present a different challenge than a binary classification problem. The skewed distribution makes many conventional machine learning algorithms less effective, especially in predicting minority class examples. In order to do so, let us first understand the problem at hand and then discuss the ways to overcome those.',
+            'author': 'Antonio Banderas',
+            'create_date': '07-14-2005'
+        }
+    ]
+    return articles
